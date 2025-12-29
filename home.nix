58f80c68bbb60251122edc0b7c4ff97a14ec9578
Masande <masande.mti@gmail.com>
2025-12-29 21:55:16 +0000

    add i3 shortcuts

--- home.nix
+++ home.nix
@@ -1,22 +1,115 @@
 { config, pkgs, ... }:
-
 {
-  home.stateVersion = "24.05"; # adjust to your NixOS version
-
+  home.stateVersion = "24.05";
   programs.i3status.enable = true;
-
   xsession.windowManager.i3 = {
     enable = true;
-
     config = {
       modifier = "Mod4";
       terminal = "alacritty";
-
-      keybindings = {
-        "Mod4+Return" = "exec alacritty";
-        "Mod4+d" = "exec dmenu_run";
+      focus.followMouse = false;
+      focus.mouseWarping = false;
+      keybindings = let
+        mod = "Mod4";
+      in {
+        # Terminal and launcher
+        "${mod}+Return" = "exec alacritty";
+        "${mod}+d" = "exec dmenu_run";
+        
+        # Focus windows (vim keys)
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+        
+        # Focus windows (arrow keys)
+        "${mod}+Left" = "focus left";
+        "${mod}+Down" = "focus down";
+        "${mod}+Up" = "focus up";
+        "${mod}+Right" = "focus right";
+        
+        # Move windows (vim keys)
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+        
+        # Move windows (arrow keys)
+        "${mod}+Shift+Left" = "move left";
+        "${mod}+Shift+Down" = "move down";
+        "${mod}+Shift+Up" = "move up";
+        "${mod}+Shift+Right" = "move right";
+        
+        # Split orientation
+        "${mod}+b" = "split h";
+        "${mod}+v" = "split v";
+        "${mod}+f" = "fullscreen toggle";
+        
+        # Layout modes
+        "${mod}+s" = "layout stacking";
+        "${mod}+w" = "layout tabbed";
+        "${mod}+e" = "layout toggle split";
+        
+        # Floating
+        "${mod}+Shift+space" = "floating toggle";
+        "${mod}+space" = "focus mode_toggle";
+        
+        # Focus parent/child
+        "${mod}+a" = "focus parent";
+        
+        # Workspaces
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+        "${mod}+6" = "workspace number 6";
+        "${mod}+7" = "workspace number 7";
+        "${mod}+8" = "workspace number 8";
+        "${mod}+9" = "workspace number 9";
+        "${mod}+0" = "workspace number 10";
+        
+        # Move to workspace
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${mod}+Shift+0" = "move container to workspace number 10";
+        
+        # Window control
+        "${mod}+Shift+q" = "kill";
+        
+        # i3 control
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+        "${mod}+Shift+e" = "exec i3-msg exit";
+        
+        # Resize mode
+        "${mod}+r" = "mode resize";
       };
-
+      
+      modes = {
+        resize = {
+          "h" = "resize shrink width 10 px or 10 ppt";
+          "j" = "resize grow height 10 px or 10 ppt";
+          "k" = "resize shrink height 10 px or 10 ppt";
+          "l" = "resize grow width 10 px or 10 ppt";
+          
+          "Left" = "resize shrink width 10 px or 10 ppt";
+          "Down" = "resize grow height 10 px or 10 ppt";
+          "Up" = "resize shrink height 10 px or 10 ppt";
+          "Right" = "resize grow width 10 px or 10 ppt";
+          
+          "Return" = "mode default";
+          "Escape" = "mode default";
+        };
+      };
+      
       startup = [
         { command = "picom"; }
         {
@@ -27,27 +120,20 @@
       ];
     };
   };
-
   home.packages = with pkgs; [
     alacritty
     dmenu
     picom
   ];
-
   programs.git = {
     enable = true;
-    
     settings = {
-      
       init.defaultBranch = "main";
       pull.rebase = true;
-
       user = {
         name = "Masande";
         email = "masande.mti@gmail.com";
       };
     };
   };
-
-
 }
